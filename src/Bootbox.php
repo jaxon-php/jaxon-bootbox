85f2d6ab402638f3b39e161bfdae7ef8540f826c
feuzeu <thierry.feuzeu@gmail.com>
2016-05-18 23:04:51 -0400

    Replaced tabs with spaces.

--- src/Bootbox.php
+++ src/Bootbox.php
@@ -4,65 +4,65 @@ namespace Xajax\Bootbox;
 
 class Bootbox extends \Xajax\Plugin\Response
 {
-	use \Xajax\Utils\ContainerTrait;
+    use \Xajax\Utils\ContainerTrait;
 
-	public function getName()
-	{
-		return 'bootbox';
-	}
+    public function getName()
+    {
+        return 'bootbox';
+    }
 
-	public function generateHash()
-	{
-		// The version number is used as hash
-		return '1.0.0';
-	}
+    public function generateHash()
+    {
+        // The version number is used as hash
+        return '1.0.0';
+    }
 
-	public function getJs()
- 	{
-		if(!$this->includeAssets())
-		{
-			return '';
-		}
- 		return  '<script type="text/javascript" src="//assets.lagdo-software.net/libs/bootbox/4.3.0/bootbox.min.js"></script>';
- 	}
+    public function getJs()
+     {
+        if(!$this->includeAssets())
+        {
+            return '';
+        }
+         return  '<script type="text/javascript" src="//assets.lagdo-software.net/libs/bootbox/4.3.0/bootbox.min.js"></script>';
+     }
 
-	public function getScript()
-	{
-		return '
+    public function getScript()
+    {
+        return '
 xajax.command.handler.register("bootbox", function(args) {
-	bootbox.alert(args.data.content);
+    bootbox.alert(args.data.content);
 });
 ';
-	}
+    }
 
-	protected function alert($title, $content, $class)
-	{
-		$content = '
-		<div class="alert alert-' . $class . '" style="margin-top:15px;margin-bottom:-15px;">
-			<strong>' . $title . '</strong>
-			' . $content . '
-		</div>
+    protected function alert($title, $content, $class)
+    {
+        $content = '
+        <div class="alert alert-' . $class . '" style="margin-top:15px;margin-bottom:-15px;">
+            <strong>' . $title . '</strong>
+            ' . $content . '
+        </div>
 ';
-		$this->addCommand(array('cmd'=>'bootbox'), array('content' => $content));
-	}
+        $this->addCommand(array('cmd'=>'bootbox'), array('content' => $content));
+    }
 
-	public function success($title, $content)
-	{
-		$this->alert($title, $content, 'success');
-	}
+    public function success($title, $content)
+    {
+        $this->alert($title, $content, 'success');
+    }
 
-	public function info($title, $content)
-	{
-		$this->alert($title, $content, 'info');
-	}
+    public function info($title, $content)
+    {
+        $this->alert($title, $content, 'info');
+    }
 
-	public function warning($title, $content)
-	{
-		$this->alert($title, $content, 'warning');
-	}
+    public function warning($title, $content)
+    {
+        $this->alert($title, $content, 'warning');
+    }
 
-	public function error($title, $content)
-	{
-		$this->alert($title, $content, 'danger');
-	}
+    public function error($title, $content)
+    {
+        $this->alert($title, $content, 'danger');
+    }
 }
